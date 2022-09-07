8f6e395c7f8d5785db2cfaa51fee5ff651e5887f
Thong Chunlee <thong.chunlee@ababank.com>
2022-09-07 16:43:24 +0700

    fix example lint

--- example/lib/widgets/example_scaffold.dart
+++ example/lib/widgets/example_scaffold.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ExampleScaffold extends StatelessWidget {
   final String title;
   final List<Widget> children;
-  const ExampleScaffold({Key? key, required this.title, required this.children})
-      : super(key: key);
+  const ExampleScaffold({Key? key, required this.title, required this.children}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
