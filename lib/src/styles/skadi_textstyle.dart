7cae5cdb793184023724bf9e882dcd72cfde5944
Chunlee Thong <chunlee.thong@gmail.com>
2022-12-26 18:52:02 +0700

    add textstyle utils

---
+++ lib/src/styles/skadi_textstyle.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kTs32 = TextStyle(fontSize: 32);
+const TextStyle kTs28 = TextStyle(fontSize: 28);
+const TextStyle kTs24 = TextStyle(fontSize: 24);
+const TextStyle kTs20 = TextStyle(fontSize: 20);
+const TextStyle kTs18 = TextStyle(fontSize: 18);
+const TextStyle kTs16 = TextStyle(fontSize: 16);
+const TextStyle kTs14 = TextStyle(fontSize: 14);
+const TextStyle kTs12 = TextStyle(fontSize: 12);
+const TextStyle kTs10 = TextStyle(fontSize: 10);
+const TextStyle kTs8 = TextStyle(fontSize: 8);
+
+TextStyle kTs(double fontSize) {
+  return TextStyle(fontSize: fontSize);
+}
