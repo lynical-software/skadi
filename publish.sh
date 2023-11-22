ada3dc9c473a81c0fed720830b00682de8c5d11f
chunlee-thong <chunlee.thong@gmail.com>
2023-11-22 14:42:01 +0700

    update publish script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ version=$1
 flutter clean && flutter pub get
 dart format .
 git add .
-git commit -m '${version} release'
+git commit -m '$version release'
 git push
 git checkout master
 git merge dev
